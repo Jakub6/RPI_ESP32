80dac661556758a6589a057dc095059845aee916
JAJ <jerabekjakub@post.cz>
2020-03-22 21:41:19 +0100

    debounce irq, timer 200ms

--- MAIN_TEST/mqqt/irq/main.py
+++ MAIN_TEST/mqqt/irq/main.py
@@ -1,4 +1,4 @@
-from machine import Pin, I2C
+from machine import Pin, I2C, Timer
 import time 
 import BME280
 import ssd1306
@@ -9,33 +9,6 @@ import ubinascii
 import micropython
 import esp
 
-mqtt_server = '192.168.1.171'
-message_interval = 5
-last_message = 0
-
-topic_sub1 = b"led"
-topic_pub1 = b"led"
-
-topic_sub = b"temp"
-topic_pub = b"temp"
-client_id = "esp-32"
-
-i2c_oled = I2C(-1, scl=Pin(22), sda=Pin(21))
-oled_width = 128
-oled_height = 64
-oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c_oled)
-
-i2c = I2C(scl=Pin(18), sda=Pin(23), freq=10000)
-bme = BME280.BME280(i2c=i2c)
-led = machine.Pin(5, machine.Pin.OUT)
-
-#button = machine.Pin(19, machine.Pin.IN, machine.Pin.PULL_UP)
-
-def extIntHandler(pin):
-	global button, client
-	button_state = not button.value()
-	led_state = 1 if button_state == True else 0
-	client.publish(topic_pub1,b"%s" %led_state)
 
 def sub_cb(topic, msg):
 	global oled
@@ -47,12 +20,14 @@ def sub_cb(topic, msg):
 		oled.text('Temp:%s' %data["temp"], 0, 20)
 		oled.text('Press:%s' %data["pres"], 0, 40)
 		oled.show()
-		print('ESP received temperature')
+
 	if topic == b'led':
 		if msg==b'1':
-			led.value(1)			
+			led.value(1)
 		else:
 			led.value(0)
+			
+
 def connect_and_subscribe():
 	global client_id, mqtt_server, topic_sub
 	client = MQTTClient(client_id, mqtt_server)
@@ -69,13 +44,58 @@ def restart_and_reconnect():
   time.sleep(10)
   machine.reset()
 
+def on_pressed(timer):
+	global client, led
+	state = led.value()
+	led_state = 0 if state == True else 1
+	client.publish(topic_pub1,b"%s" %led_state)
+
+def debounce(pin):
+    # Start or replace a timer for 200ms, and trigger on_pressed.
+    timer.init(mode=Timer.ONE_SHOT, period=200, callback=on_pressed)
+
+# Address MQQT broker server running on RPI
+mqtt_server = '192.168.1.171'
+
+# Sending mqqt msg in each message interval
+message_interval = 5
+last_message = 0
+
+# MQQT topics
+topic_sub1 = b"led"
+topic_pub1 = b"led"
+
+topic_sub = b"temp"
+topic_pub = b"temp"
+client_id = "esp-32"
+
+# I2C for OLED display and inicialization
+i2c_oled = I2C(-1, scl=Pin(22), sda=Pin(21))
+# 
+oled_width = 128
+oled_height = 64
+oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c_oled)
+
+# I2C for senzor BME280 and inicialization 
+i2c = I2C(scl=Pin(18), sda=Pin(23), freq=10000)
+bme = BME280.BME280(i2c=i2c)
+
+# Init Led
+led = machine.Pin(5, machine.Pin.OUT)
+
+# Register a new hardware timer.
+timer = Timer(0)
+
+# Setup the button input pin with a pull-up resistor.
+button = Pin(19, Pin.IN, Pin.PULL_UP)
+
+# Register an interrupt on rising button input.
+button.irq(debounce, Pin.IRQ_RISING)
+
 try:
   client = connect_and_subscribe()
 except OSError as e:
   restart_and_reconnect()
-  
-button = machine.Pin(19, machine.Pin.IN, machine.Pin.PULL_UP)
-button.irq(trigger=machine.Pin.IRQ_FALLING, handler=extIntHandler)
 
 while True:
 	try:
@@ -90,3 +110,6 @@ while True:
 			  last_message = time.time()
 	except OSError as e:
 		restart_and_reconnect()
+		
+
+
